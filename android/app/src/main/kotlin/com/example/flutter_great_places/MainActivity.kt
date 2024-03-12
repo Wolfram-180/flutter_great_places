90e73f45d5be9c34afa27d60036427d71b4abe84
Wolfram-180 <sergei.ulvis@gmail.com>
2024-03-12 17:49:16 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_great_places/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_great_places
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_great_places/helpers/location_helper.dart';
+import 'package:flutter_great_places/screens/map_screen.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  final Function onSelectPlace;
+
+  LocationInput(this.onSelectPlace);
+
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  void _showPreview(double? lat, double? lng) {
+    final staticMapImagUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: lat, longitude: lng);
+    setState(() {
+      _previewImageUrl = staticMapImagUrl;
+    });
+  }
+
+  Future<void> _getCurrentUserLocation() async {
+    try {
+      final locData = await Location().getLocation();
+      _showPreview(locData.latitude, locData.longitude);
+      widget.onSelectPlace(locData.latitude, locData.longitude);
+    } catch (error) {
+      return;
+    }
+  }
+
+  Future<void> _selectOnMap() async {
+    final LatLng? selectedLocation = await Navigator.of(context).push<LatLng>(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
+    _showPreview(selectedLocation.latitude, selectedLocation.longitude);
+    widget.onSelectPlace(selectedLocation.latitude, selectedLocation.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration:
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          child: _previewImageUrl == null
+              ? const Text(
+                  'No location chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              icon: Icon(
+                Icons.location_on,
+              ),
+              onPressed: _getCurrentUserLocation,
+              label: Text('Current location'),
+              //textColor: Theme.of(context).primaryColor,
+            ),
+            TextButton.icon(
+              icon: Icon(
+                Icons.map,
+              ),
+              onPressed: _selectOnMap,
+              label: Text('Select on map'),
+              //textColor: Theme.of(context).primaryColor,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
